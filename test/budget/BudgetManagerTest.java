52c34dd9586b87b55dbecc95e45c2dfb0b37cfa3
ernest nnodu <ernest.nnodu@outlook.com>
2024-07-17 21:33:42 +0100

    Completed Purchase and BudgetManager Class

---
+++ test/budget/BudgetManagerTest.java
@@ -0,0 +1,55 @@
+package budget;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BudgetManagerTest {
+
+    private BudgetManager budgetManager;
+
+    @Before
+    public void setUp() {
+        budgetManager = new BudgetManager();
+    }
+
+    @Test
+    @DisplayName("Budget Manager Initialised successfully")
+    public void budgetManagerInitTest() {
+        List<Purchase> purchases = budgetManager.getPurchases();
+
+        assertEquals(0, purchases.size());
+    }
+
+    @Test
+    @DisplayName("Purchases added successfully")
+    public void addPurchasesTest() {
+        budgetManager.addPurchase(new Purchase("item1", 23.50));
+        budgetManager.addPurchase(new Purchase("item2", 67.0));
+        budgetManager.addPurchase(new Purchase("item3", 509));
+
+        List<Purchase> purchases = budgetManager.getPurchases();
+
+        assertEquals(3, purchases.size());
+        assertEquals("item1", purchases.get(0).getName());
+        assertEquals(23.50, purchases.get(0).getAmount());
+        assertEquals("item2", purchases.get(1).getName());
+        assertEquals(67, purchases.get(1).getAmount());
+        assertEquals("item3", purchases.get(2).getName());
+        assertEquals(509, purchases.get(2).getAmount());
+    }
+
+    @Test
+    @DisplayName("Calculate total amount of purchases")
+    public void calculateTotalAmountTest() {
+        budgetManager.addPurchase(new Purchase("item1", 23.50));
+        budgetManager.addPurchase(new Purchase("item2", 67.0));
+        budgetManager.addPurchase(new Purchase("item3", 509));
+
+        assertEquals(599.50, budgetManager.calculateTotal());
+    }
+}
--- test/budget/PurchaseTest.java
+++ test/budget/PurchaseTest.java
@@ -1,7 +1,31 @@
 package budget;
 
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class PurchaseTest {
+public class PurchaseTest {
 
+    @Test
+    @DisplayName("Object successfully created")
+    public void testConstructorsAndGetters() {
+
+        Purchase purchase = new Purchase("item", 10.75);
+
+        assertEquals("item", purchase.getName());
+        assertEquals(10.75, purchase.getAmount(), 0.001);
+    }
+
+    @Test
+    @DisplayName("Object successfully modified")
+    public void testConstructorsAndSetters() {
+
+        Purchase purchase = new Purchase("item", 10.75);
+        purchase.setName("Apple");
+        purchase.setAmount(43.17);
+
+        assertEquals("Apple", purchase.getName());
+        assertEquals(43.17, purchase.getAmount(), 0.001);
+    }
 }
